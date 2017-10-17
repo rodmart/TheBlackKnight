049ac9f60602fcd194b81b4bfd24b37c2cca44a0
Carlos <lilaznman424@gmail.com>
2017-10-16 19:55:43 -0700

    Carlos' push
    Set up the player
    set up a NPC to test and build with
    NPC has audio when close enough, and quest appear above head. Looking to improve this

---
+++ Assets/Scripts/Villager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Villager : MonoBehaviour {
+
+    public float rangeDistance;
+    public Transform villager;
+    public Transform player;
+	private int playOnce = 0;
+	public Text quest1;
+	
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		Canvas quest = GetComponent<Canvas>();
+		AudioSource audio = GetComponent<AudioSource>();
+
+        if ((Distance() <= rangeDistance) && (playOnce == 0))
+        {
+            audio.Play();
+			playOnce=1;
+			setQuest();
+        }
+		
+		if ((Distance() > rangeDistance) && (playOnce == 1))
+        {
+			playOnce=0;
+			deleteQuest();
+        }
+
+
+
+    }
+	
+	private void setQuest(){
+		quest1.text="I need your help!";
+	}
+	
+	private void deleteQuest(){
+		quest1.text = "";
+	}
+	
+	
+	private float Distance()
+    {
+
+        return Vector3.Distance(player.position, villager.position);
+    }
+}
