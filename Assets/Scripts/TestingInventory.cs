dfebb9f4c2a354c0b6f5a0e4262fd31b69d6cb4d
RM <ghost@RMs-MacBook-Air.local>
2017-12-12 22:40:27 -0800

    added pause and return to menu: press P to PAUSE, press Q to return to main menu

--- Assets/Scripts/TestingInventory.cs
+++ Assets/Scripts/TestingInventory.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TestingInventory : MonoBehaviour {
     public GameObject inventoryUI;
+    public bool paused;
     // Use this for initialization
     void Start () {
         inventoryUI.SetActive(false);
+        paused=false;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q)){
+            SceneManager.LoadScene("Menu");
+        }
 
         if(inventoryUI.activeSelf)
         {
@@ -22,6 +28,18 @@ public class TestingInventory : MonoBehaviour {
             if (Input.GetKeyDown(KeyCode.I))
                 inventoryUI.SetActive(true);
         }
+       if (Input.GetKeyDown(KeyCode.P)){ 
+                 paused = !paused;
+             }
+        
+        if (paused) {
+            Time.timeScale = 0f;
+            }
+         else if(!paused)
+         {
+            Time.timeScale=1f;
+         }
+        
 
     }
 }
